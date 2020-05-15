d232b682d1324819836c00629fc7373d254bd354
Elsayed Rashed <elsayed.rashed@gmail.com>
2020-05-16 01:10:14 +0400

    Initial commit

--- main/main.py
+++ main/main.py
@@ -1,17 +1,16 @@
 """
    Union of two dataframes.
 
-   @author rambabu.posa
 """
-import sparkUtility
+import utility.sparkUtility.dataframeUtility as su
 from pyspark.sql import SparkSession
 import os
 
 current_dir = os.path.dirname(__file__)
-relative_path1 = "../../resources/data/Restaurants_in_Wake_County_NC.csv"
+relative_path1 = "../resources/data/Restaurants_in_Wake_County_NC.csv"
 absolute_file_path1 = os.path.join(current_dir, relative_path1)
 
-relative_path2 = "../../resources/data/Restaurants_in_Durham_County_NC.json"
+relative_path2 = "../resources/data/Restaurants_in_Durham_County_NC.json"
 absolute_file_path2 = os.path.join(current_dir, relative_path2)
 
 # Creates a session on a local master
@@ -23,7 +22,7 @@ df1 = spark.read.csv(path=absolute_file_path1,header=True,inferSchema=True)
 df2 = spark.read.json(absolute_file_path2)
 
 
-wakeRestaurantsDf = util.build_wake_restaurants_dataframe(df1)
-durhamRestaurantsDf = util.build_durham_restaurants_dataframe(df2)
+wakeRestaurantsDf = su.build_wake_restaurants_dataframe(df1)
+durhamRestaurantsDf = su.build_durham_restaurants_dataframe(df2)
 
-util.combineDataframes(wakeRestaurantsDf, durhamRestaurantsDf)
+su.combineDataframes(wakeRestaurantsDf, durhamRestaurantsDf)
