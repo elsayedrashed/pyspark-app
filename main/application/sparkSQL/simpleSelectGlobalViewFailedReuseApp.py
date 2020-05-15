cf700c3724041e99e46c28cd27662fa410ca0d57
Elsayed Rashed <elsayed.rashed@gmail.com>
2020-05-16 01:48:37 +0400

    SparkSQL commit

---
+++ main/application/sparkSQL/simpleSelectGlobalViewFailedReuseApp.py
@@ -0,0 +1,31 @@
+"""
+  Simple SQL select on ingested data
+
+To test:
+ * Run simpleSelectGlobalViewApp first
+ * You have 60s to run simpleSelectGlobalViewFailedReuseApp, but you will see that the view is not shared between the application...
+
+"""
+
+from pyspark.sql import SparkSession
+
+# Creates a session on a local master
+spark = SparkSession.builder.appName("Simple SELECT using SQL") \
+    .master("local[*]") \
+    .getOrCreate()
+
+query = """
+  SELECT * FROM global_temp.geodata
+  WHERE yr1980 > 1
+  ORDER BY 2
+  LIMIT 5
+"""
+
+# This will fail as it is not the same application
+smallCountries = spark.sql(query)
+
+# Shows at most 10 rows from the dataframe (which is limited to 5 anyway)
+smallCountries.show(10, False)
+
+# Good to stop SparkSession at the end of the application
+spark.stop()
