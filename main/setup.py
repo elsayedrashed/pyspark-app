b5b1e234df3931e3c883e2a46e85f80c0169bf80
Elsayed Rashed <elsayed.rashed@gmail.com>
2020-05-17 01:25:01 +0400

    Pyspark in Action commit

---
+++ main/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyspark-app',
+    version='',
+    packages=['utility', 'utility.sparkUtility', 'application', 'application.sparkSQL'],
+    package_dir={'': 'main'},
+    url='',
+    license='',
+    author='elsay',
+    author_email='',
+    description=''
+)
