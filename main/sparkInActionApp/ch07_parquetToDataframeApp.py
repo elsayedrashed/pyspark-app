ed0adcf104a7a581477164b08407de7fb32d3c09
Elsayed Rashed <elsayed.rashed@gmail.com>
2020-09-13 01:39:56 +1000

    Pyspark in Action commit

--- main/sparkInActionApp/ch07_parquetToDataframeApp.py
+++ main/sparkInActionApp/ch07_parquetToDataframeApp.py
@@ -9,21 +9,32 @@
 from pyspark.sql import SparkSession
 import os
 
-current_dir = os.path.dirname(__file__)
-relative_path = "../../resources/data/sparkInActionData/alltypes_plain.parquet"
-absolute_file_path = os.path.join(current_dir, relative_path)
+# Creates a session
+def create_spark_session(appname, master):
+    return SparkSession.builder.appName(appname).master(master).getOrCreate()
 
-# Creates a session on a local master
-spark = SparkSession.builder.appName("Parquet to Dataframe") \
-    .master("local[*]").getOrCreate()
+# Get absolute file path
+def get_absolute_file_path(path, filename):
+    current_dir = os.path.dirname(__file__)
+    relative_path = "{}/{}".format(path, filename)
+    return os.path.join(current_dir, relative_path)
 
-# Reads a Parquet file, stores it in a dataframe
-df = spark.read.format("parquet") \
-    .load(absolute_file_path)
+# Main function
+def main():
+    path = "../../resources/data/sparkInActionData"
+    filename = "alltypes_plain.parquet"
+    absolute_file_path = get_absolute_file_path(path, filename)
+    # Creat Spark Session
+    spark = create_spark_session("Parquet to Dataframe", "local[*]")
+    # Reads an parquet file, stores it in a dataframe
+    df = spark.read.format("parquet") \
+              .load(absolute_file_path)
 
-# Shows at most 10 rows from the dataframe
-df.show(10)
-df.printSchema()
-print("The dataframe has {} rows.".format(df.count()))
+    # Shows at most 10 rows from the dataframe
+    df.show(10)
+    df.printSchema()
+    print("The dataframe has {} rows.".format(df.count()))
+    spark.stop()
 
-spark.stop()
+if __name__ == "__main__":
+    main()
